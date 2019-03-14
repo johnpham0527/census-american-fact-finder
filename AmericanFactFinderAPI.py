8330a594c7a5353800583a202602738c44d2ba45
John Pham <47116221+johnpham0527@users.noreply.github.com>
2019-03-14 15:47:15 -0400

    Add files via upload
    Updated code to export American Community Survey data based on one or more given census tracts.

--- AmericanFactFinderAPI.py
+++ AmericanFactFinderAPI.py
@@ -32,6 +32,9 @@
 # Enter this URL into a web browser to explore population data for Queens ZIP Code 11432: 
 # http://factfinder.census.gov/service/data/v1/en/programs/ACS/datasets/17_5YR/tables/B01003/data/8600000US11432?maxResults=10&key=ea46e190165e1ee608d643fba987f8b3620ec1a9
 
+# Enter this URL into a web browser to explore population data for Queens County Census Tract 25: 
+# http://factfinder.census.gov/service/data/v1/en/programs/ACS/datasets/17_5YR/tables/B01003/data/1400000US36081002500?maxResults=10&key=ea46e190165e1ee608d643fba987f8b3620ec1a9
+
 
 import requests
 import json
@@ -85,16 +88,78 @@ def outputDatabyZIPCodeList(ZIPCodeFileName, CSVoutputFileName, year, tableNumbe
             outputWriter.writerow([zipCodeNoWhiteSpace,ZIPCodeValue]) #write the ZIP code and value to the CSV output file
         outputFile.close() #close the file
 
+def getAmericanCommunitySurvey5YearEstimateValueByCensusTract(year, tableNumber, censusTract):
+    #build the URL string, given the parameters
+    getUrl = "http://factfinder.census.gov/service/data/v1/en/programs/ACS/datasets/" + year + "_5YR/tables/" + tableNumber + "/data/1400000US" + censusTract
+    parameters = {"maxResults":1, "key":"ea46e190165e1ee608d643fba987f8b3620ec1a9"} #parameters for the URL
+    requestResult = requests.get(getUrl,params=parameters) #submit the GET request
+    resultText = requestResult.text #obtain the requested text
+    jsonText = json.loads(resultText) #convert the requested text to JSON format
+
+    if (tableNumber == "B01003"): #B01003 refers to the American Community Survey dataset for the population estimate
+        tableKey = tableNumber + "_1_EST" #build a string that will serve as the key for the JSON key-value pair
+        return jsonText["data"]["rows"][0]["cells"][tableKey]["value"] #return the population estimate
+
+    elif (tableNumber == "S2301"): #S2301 refers to the American Community Survey dataset for the unemployment rate
+        tableKey = "C7" #C7 refers to the unemployment rate for the population age 16 and over
+        return jsonText["data"]["rows"][0]["cells"][tableKey]["value"] #return the unemployment rate for the population age 16 and over
+
+    elif (tableNumber == "S1501"):#S1501 refers to the American Community Survey dataset for educational attainment
+        tableKey = "C75" #this refers to the percentage of people age 25+ who have less than a 9th grade education
+        resultAge25OlderLessThan9thGrade = jsonText["data"]["rows"][0]["cells"][tableKey]["value"] #obtain value
+        tableKey = "C87" #this refers to the percentage of people age 25+ who have a 9th-12th grade education but lack a high school diploma or its equivalent
+        resultAge25Older9thto12thNoDiploma = jsonText["data"]["rows"][0]["cells"][tableKey]["value"] #obtain value
+        #The function is looking to return the sum of C75 and C87:
+        return resultAge25OlderLessThan9thGrade + resultAge25Older9thto12thNoDiploma #return the total percentage of people age 25+ who do not have a high school diploma or its equivalent
+
+    elif (tableNumber == "S1701"): #S1701 refers to the American Community Survey dataset for poverty
+        tableKey = "C5" #C5 refers to the percentage of population for whom poverty status is determined
+        return jsonText["data"]["rows"][0]["cells"][tableKey]["value"]
+
+    elif (tableNumber == "DP02"): #DP02 refers to the American Community Survey dataset called "Selected Social Characteristics in the United States"
+        tableKey = "C411" #C411 refers to the percentage of people age 5 and older who speak English less than "very well," among those people who speak a language other than English
+        return jsonText["data"]["rows"][0]["cells"][tableKey]["value"]
+    
+    else:
+        print("Error: This code cannot handle Table Number " + tableNumber)
+
+def outputDatabyCensusTractList(censusTractFileName, CSVoutputFileName, year, tableNumber):
+    with open(censusTractFileName, encoding = 'utf-8') as inputFile:
+        outputFile = open(CSVoutputFileName,'w',newline=None) #open an output file for writing
+        outputWriter = csv.writer(outputFile) #initiate the CSV writer object
+        for censusTract in inputFile: #for each ZIP code in the input file
+            censusTractNum = int(censusTract) #this line removes the line break in the ZIP code
+            censusTractString = str(censusTractNum) #convert to a string
+            censusTractValue = getAmericanCommunitySurvey5YearEstimateValueByCensusTract(year, tableNumber, censusTractString)
+            print(str(censusTractString) + " " + str(censusTractValue))
+            censusTractNoWhiteSpace = censusTract.rstrip() #remove trailing white space
+            outputWriter.writerow([censusTractNoWhiteSpace, censusTractValue]) #write the ZIP code and value to the CSV output file
+        outputFile.close() #close the file
+
 # Uncomment these lines to run sample queries
 #print(getAmericanCommunitySurvey5YearEstimateValue("17","B01003","11432")) #population count
-print("Statistics for ZIP Code 11432")
-print("Unemployment Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S2301","11432"),'.1f')))  #unemployment rate. The code limits the output to one decimal place
-print("Percent without a High School Diploma or Equivalent: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S1501","11432"),'.1f'))) #percentage of people without a HS diploma or equivalency
-print("Poverty Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S1701","11432"),'.1f'))) #poverty rate
-print("Percent with Limited English Language Proficiency: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","DP02","11432"),'.1f'))) #limited English language proficiency
+#print("Statistics for ZIP Code 11432")
+#print("Unemployment Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S2301","11432"),'.1f')))  #unemployment rate. The code limits the output to one decimal place
+#print("Percent without a High School Diploma or Equivalent: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S1501","11432"),'.1f'))) #percentage of people without a HS diploma or equivalency
+#print("Poverty Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","S1701","11432"),'.1f'))) #poverty rate
+#print("Percent with Limited English Language Proficiency: " + str(format(getAmericanCommunitySurvey5YearEstimateValue("17","DP02","11432"),'.1f'))) #limited English language proficiency
 
 # Uncomment these lines to generate a CSV file of values based on an input file containing a list of ZIP codes
 #outputDatabyZIPCodeList("QueensZIPCodes.txt","UnemploymentZIP.csv","17","S2301")
 #outputDatabyZIPCodeList("QueensZIPCodes.txt","NoHighSchoolZIP.csv","17","S1501")
 #outputDatabyZIPCodeList("QueensZIPCodes.txt","PovertyZIP.csv","17","S1701")
 #outputDatabyZIPCodeList("QueensZIPCodes.txt","LimitedEnglishZIP.csv","17","DP02")
+
+#Queens County Census Tract 2500 = Queensbridge (New York State's ID = 36 and Queens County's ID is 81)
+censusTract = "36081002500"
+print("Statistics for Census Tract 2500 (Queensbridge) in Queens County, NY")
+print("Unemployment Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValueByCensusTract("17","S2301",censusTract),'.1f')))  #unemployment rate. The code limits the output to one decimal place
+print("Percent without a High School Diploma or Equivalent: " + str(format(getAmericanCommunitySurvey5YearEstimateValueByCensusTract("17","S1501",censusTract),'.1f'))) #percentage of people without a HS diploma or equivalency
+print("Poverty Rate: " + str(format(getAmericanCommunitySurvey5YearEstimateValueByCensusTract("17","S1701",censusTract),'.1f'))) #poverty rate
+print("Percent with Limited English Language Proficiency: " + str(format(getAmericanCommunitySurvey5YearEstimateValueByCensusTract("17","DP02",censusTract),'.1f'))) #limited English language proficiency
+
+# Uncomment these lines to generate a CSV file of values based on an input file containing a list of ZIP codes
+#outputDatabyCensusTractList("NYCHAQueensCensusTracts.txt","UnemploymentCensusTract.csv","17","S2301")
+#outputDatabyCensusTractList("NYCHAQueensCensusTracts.txt","NoHighSchoolCensusTract.csv","17","S1501")
+#outputDatabyCensusTractList("NYCHAQueensCensusTracts.txt","PovertyCensusTract.csv","17","S1701")
+#outputDatabyCensusTractList("NYCHAQueensCensusTracts.txt","LimitedEnglishCensusTract.csv","17","DP02")
